e9c372fcb717440bec5267fde740eb682963fc6b
vuqaraslan <vuqaraovmail@gmail.com>
2024-12-20 20:58:45 +0400

    git actions

---
+++ TestGitActions_DevOpsls5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestGitActions_DevOpsls5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
